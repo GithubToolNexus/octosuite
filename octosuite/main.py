af753ca44c49c249ae1b841090aee022d4b5bf23
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-06-30 01:27:21 +0200

    Update main.py

--- octosuite/main.py
+++ octosuite/main.py
@@ -1,53 +1,70 @@
-# import everything from the octosuite.py file
-from octosuite.octosuite import *  # I drifted away from the 'pythonic way' here
+from octosuite.octosuite import *
+from octosuite.miscellaneous import path_finder, check_updates
+from octosuite.config import setup_cli_completion, configure_logging, create_parser
+
+setup_cli_completion()
+
+parser = create_parser()
+args = parser.parse_args()
 
 
-def octosuite():
-    setup_readline()
+def run() -> None:
+    """
+    Run the Octosuite application.
+
+    This function initializes the Octosuite instance, sets up necessary paths, configures logging,
+    checks for updates, and handles command line arguments and user input.
+
+    :return: None
+    """
     try:
-        run = Octosuite()
-        path_finder()
+        octosuite = Octosuite()
+        path_finder([".logs", "output", "downloads"])
         configure_logging()
         check_updates()
         if args.method:
             """
-            Iterate over the argument_map and check if the passed command line argument matches any argument in it [argument_map],
-            if there's a match, we return its method. If no match is found, we do nothing (which will return the usage).
+            Iterate over the argument_map and check if the passed command line argument matches any argument in it
+            [argument_map].
+            If there's a match, execute its associated method.
+            If no match is found, do nothing (which will return the usage).
             """
-            for argument, method in run.argument_map:
+            for argument, method in octosuite.argument_map:
                 if args.method == argument:
-                    method()
+                    method(args)
                     print("\n")
                 else:
                     pass
         else:
             """
-            Main loop keeps octosuite running, this will break if Octosuite detects a KeyboardInterrupt (Ctrl+C)
+            Main loop to keep Octosuite running. The loop will break if Octosuite detects a KeyboardInterrupt (Ctrl+C)
             or if the 'exit' command is entered.
             """
-            xprint(banner()[0], banner()[1])
             while True:
-                command_input = Prompt.ask(f"{white}┌──({red}{getpass.getuser()}{white}@{red}octosuite{white})\n├──[~{green}{os.getcwd()}{white}]\n└╼{reset}")
+                command_input = Prompt.ask(f"{getpass.getuser()}@:octopus: [blue]{os.getcwd()}[/]")
                 """
-                Iterate over the command_map and check if the user input matches any command in it [command_map],
-                if there's a match, we return its method. If no match is found, we ignore it.
+                Iterate over the command_map and check if the user input matches any command in it [command_map].
+                If there's a match, execute its associated method. If no match is found, ignore it.
                 """
                 if command_input[:2] == 'cd':
                     os.chdir(command_input[3:])
                 elif command_input[:2] == 'ls':
-                    os.system(f'dir {command_input[3:]}' if os.name == 'nt' else f'ls {command_input[3:]}')
+                    list_files_and_directories(command_input[3:])
                 else:
-                    for command, method in run.command_map:
+                    """
+                    Execute the specified method based on the user input.
+                    If no match is found, ignore it.
+                    """
+                    for command, method in octosuite.command_map:
                         if command_input == command:
-                            method()
-                            print("\n")
+                            method(args)
                         else:
                             pass
-        
+
     except KeyboardInterrupt:
-        logging.warning(ctrl_c)
-        xprint(f"\n{WARNING} {ctrl_c}")
+        logging.warning(message.ctrl_c())
+        xprint(message.ctrl_c())
 
     except Exception as e:
-        logging.error(error.format(e))
-        xprint(f"{ERROR} {error.format(e)}")
+        logging.error(message.error(e))
+        xprint(message.error(e))
