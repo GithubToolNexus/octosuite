39cf30b74727d15ad4d357c960d5fc2cc08d6afd
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-07-13 22:29:35 +0200

    Update miscellaneous.py

--- octosuite/miscellaneous.py
+++ octosuite/miscellaneous.py
@@ -1,24 +1,18 @@
 import os
-import json
 import psutil
 import platform
+import requests
 import subprocess
 from rich import print as xprint
 from rich.markdown import Markdown
-from urllib.request import urlopen
-from octosuite.config import Colours
 from octosuite.config import Version
-from octosuite.messages import Messages
+from octosuite.messages import Message
 
-from urllib.error import HTTPError, URLError
-
-
-colour = Colours()
-message = Messages()
-version = Version()
+__message = Message()
+__version = Version()
 
 
-def path_finder(directories: list) -> None:
+def path_finder(directories: list):
     """
     Checks if the specified directories exist.
     If not, it creates them.
@@ -26,12 +20,10 @@ def path_finder(directories: list) -> None:
     :param directories: List of directories to check and create
     :return: None
     """
-    # Construct path to the user's home directory
-    home_directory = os.path.expanduser("~")
 
     for directory in directories:
         # Construct and create each directory from the directories list if it doesn't already exist
-        os.makedirs(os.path.join(home_directory, directory), exist_ok=True)
+        os.makedirs(directory, exist_ok=True)
 
 
 def systeminfo() -> dict:
@@ -65,24 +57,33 @@ def systeminfo() -> dict:
     }
 
 
-def send_request(endpoint: str) -> tuple:
+def send_request(endpoint: str, authentication=None) -> tuple:
     """
     Sends a GET request to the specified endpoint and returns the status code and JSON response.
 
     :param endpoint: URL endpoint to send the request to
+    :param authentication: Authentication information for the request.
     :return: Tuple containing the status code (int) and response data (dict)
     """
+    from octosuite.config import setup_activity_logging
+
+    log = setup_activity_logging()
     try:
-        with urlopen(endpoint) as response:
-            response_data = json.loads(response.read().decode())
-            status_code = response.status
+        with requests.get(endpoint, auth=authentication,
+                          headers={"Accept": "application/vnd.github.v3+json"}) as response:
+            if authentication:
+                response_data = response.text
+            else:
+                response_data = response.json()
+            status_code = response.status_code
+
         return status_code, response_data
-    except (URLError, HTTPError) as exception:
-        xprint(f": {colour.RED}{message.error(exception)}{colour.RESET}")
-        return None, None
+    except Exception as e:
+        log.error(__message.error_occurred(exception=str(e)))
+        xprint(__message.error_occurred(exception=str(e)))
 
 
-def list_files_and_directories(directory: str = None) -> None:
+def list_files_and_directories(directory: str = None):
     """
     List contents of the specified directory or the current directory.
 
@@ -97,7 +98,7 @@ def list_files_and_directories(directory: str = None) -> None:
     subprocess.run(command)
 
 
-def check_updates() -> None:
+def check_updates():
     """
     Check for updates to Octosuite.
 
@@ -108,17 +109,18 @@ def check_updates() -> None:
     """
 
     response = send_request("https://api.github.com/repos/bellingcat/octosuite/releases/latest")
-    if response[1]['tag_name'] == version.full_version():
+    if response[1]['tag_name'] == __version.full_version():
         pass
     else:
         raw_release_notes = response[1]['body']
         markdown_release_notes = Markdown(raw_release_notes)
-        xprint(f"[{colour.GREEN}UPDATE{colour.RESET}] Octosuite v{response[1]['tag_name']} is available.")
+        xprint(__message.update_found(version_tag=response[1]['tag_name']))
         xprint(markdown_release_notes)
-        xprint("\nRun `pip install --upgrade octosuite` to install the updates.")
+        xprint(f"\nRun `{__message.colour.GREEN}pip install --upgrade "
+               f"octosuite{__message.colour.RESET}` to install the updates.")
 
 
-def clear_screen() -> None:
+def clear_screen():
     """
     Clears the screen with `cls` command if system is Windows.
     Otherwise, uses the `clear` command.
@@ -128,17 +130,18 @@ def clear_screen() -> None:
     subprocess.call('cmd.exe /c cls' if os.name == "nt" else 'clear')
 
 
-def about() -> None:
+def about():
     """
     Prints the program's about information.
 
     :return: None
     """
     about_text = """
-    OCTOSUITE © 2023 Richard Mwewa
+    OCTOSUITE/OCTOSUITE-CLI © 2022-2023 Richard Mwewa
     
-    An advanced and lightning fast framework for gathering open-source intelligence on GitHub users and organisations.
+    An all-in-one framework for gathering open-source intelligence on GitHub users and organisations.
     
+    GNU General Public License v3 (GPLv3)
     Read the wiki: https://github.com/bellingcat/octosuite/wiki
     GitHub REST API documentation: https://docs.github.com/rest
     """
