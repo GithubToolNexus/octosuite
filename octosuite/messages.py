87441726930f547f41ca4aa16fb9107bbf53dad8
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-07-13 22:25:12 +0200

    Update messages.py

--- octosuite/messages.py
+++ octosuite/messages.py
@@ -1,156 +1,104 @@
-class Messages:
-    @staticmethod
-    def ctrl_c() -> str:
-        """
-        Returns the message for session termination with Ctrl+C.
-        
-        :return: Message string
-        """
-        return "Session terminated with Ctrl+C."
+import os
+import getpass
+
+
+class Message:
+    from octosuite.config import Colours, Emojis, create_parser
+
+    colour = Colours(enable_colours=create_parser().parse_args().colours)
+
+    def __init__(self):
+        self.__emoji = Message.Emojis()
+
+    def ctrl_c(self) -> str:
+        return f"{self.__emoji.STOP_SIGN} {Message.colour.YELLOW}Session terminated with Ctrl+C.{Message.colour.RESET}"
+
+    def update_found(self, version_tag: str) -> str:
+        return f"{self.__emoji.EXCLAMATION_MARK} {Message.colour.WHITE}Octosuite " \
+               f"{Message.colour.GREEN}v{version_tag}{Message.colour.WHITE}" \
+               f" is available.{Message.colour.RESET}"
+
+    def error_occurred(self, exception: str) -> str:
+        return f"{self.__emoji.NO_ENTRY} {Message.colour.WHITE}An error occurred:{Message.colour.RESET} " \
+               f"{Message.colour.RED}{exception}{Message.colour.RESET}"
+
+    def prompt_close_session(self) -> str:
+        return f"{self.__emoji.RED_QUESTION_MARK} {Message.colour.WHITE}" \
+               f"This will closed the current session. Continue?{Message.colour.RESET}"
+
+    def prompt_clear_files(self, files_count: int) -> str:
+        return f"{self.__emoji.RED_QUESTION_MARK} " \
+               f"{Message.colour.WHITE}This will clear all " \
+               f"{Message.colour.CYAN}{files_count}{Message.colour.WHITE} files. " \
+               f"Continue?{Message.colour.RESET}"
 
     @staticmethod
-    def error(exception: str) -> str:
-        """
-        Returns the error message with the provided exception.
+    def command_prompt() -> str:
+        return f"{getpass.getuser()}@:octopus: {Message.colour.BLUE}{os.getcwd()}{Message.colour.RESET}"
 
-        :param exception: Raised exception
-        :return: Message string
-        """
-        return f"An error occurred: {exception}"
+    def unknown_command(self, command: str) -> str:
+        return f"{self.__emoji.GREEN_CROSS_MARK} {Message.colour.WHITE}Unknown command{Message.colour.RESET}: " \
+               f"{Message.colour.GREEN}{command}{Message.colour.WHITE}." \
+               f" Enter {Message.colour.GREEN}help{Message.colour.WHITE} " \
+               f"for a list of available commands.{Message.colour.RESET}"
 
-    @staticmethod
-    def session_opened(host: str, username: str) -> str:
-        """
-        Returns the message for opening a new session with the given host and username.
+    def session_opened(self, host: str, username: str) -> str:
+        return f"{self.__emoji.GREEN_CHECK_MARK} " \
+               f"{Message.colour.WHITE}Opened new session on {Message.colour.RESET}{host}:{username}"
 
-        :param host: System hostname
-        :param username: Current user's login name
-        :return: Message string
-        """
-        return f"Opened new session on {host}:{username}"
+    def session_closed(self, timestamp: str) -> str:
+        return f"{self.__emoji.GREEN_CHECK_MARK} " \
+               f"{Message.colour.WHITE}Session closed at{Message.colour.RESET} {timestamp}."
 
-    @staticmethod
-    def session_closed(timestamp: str) -> str:
-        """
-        Returns the message for session closure at the provided timestamp.
+    def file_deleted(self, filename: str) -> str:
+        return f"{self.__emoji.GREEN_CHECK_MARK} File deleted: {filename}"
 
-        :param timestamp: Current date and time
-        :return: Message string
-        """
-        return f"Session closed at {timestamp}."
+    def files_cleared(self, files_directory: str) -> str:
+        files = os.listdir(files_directory)
+        return f"{self.__emoji.GREEN_CHECK_MARK} Files cleared successfully: {files}"
 
-    @staticmethod
-    def viewing_logs() -> str:
-        """
-        Returns the message for viewing logs.
-        
-        :return: Message string
-        """
-        return "Viewing logs..."
+    def unable_to_clear_files(self, files_directory: str) -> str:
+        files = os.listdir(files_directory)
+        return f"{self.__emoji.GREEN_CROSS_MARK} Unable to clear files: {files}"
 
-    @staticmethod
-    def viewing_csv() -> str:
-        """
-        Returns the message for viewing CSV files.
-        
-        :return: Message string
-        """
-        return "Viewing CSV files..."
+    def unable_to_delete_file(self, filename: str) -> str:
+        return f"{self.__emoji.GREEN_CROSS_MARK} Unable to delete file: {filename}"
 
-    @staticmethod
-    def deleted(filename: str) -> str:
-        """
-        Returns the message for file deletion with the given filename.
+    def unable_to_read_file(self, filename: str) -> str:
+        return f"{self.__emoji.GREEN_CROSS_MARK} Unable to delete file: {filename}"
 
-        :param filename: File to delete
-        :return: Message string
-        """
-        return f"Deleted: {filename}"
+    def information_not_found(self, param1: str, param2: str, param3: str) -> str:
+        return f"{self.__emoji.GREEN_CROSS_MARK} {Message.colour.GREEN}{param1}{Message.colour.WHITE} -> " \
+               f"{Message.colour.GREEN}{param2}{Message.colour.WHITE} -> " \
+               f"{Message.colour.GREEN}{param3}{Message.colour.WHITE}" \
+               f" (information) {Message.colour.RED}Not Found{Message.colour.RESET}"
 
-    @staticmethod
-    def reading(filename: str) -> str:
-        """
-        Returns the message for reading a file with the given filename.
+    def user_not_found(self, username: str) -> str:
+        return f"{self.__emoji.NO_ENTRY} @{Message.colour.GREEN}{username}{Message.colour.WHITE} (user) " \
+               f"{Message.colour.RED}Not Found{Message.colour.RESET}."
 
-        :param filename: File to read
-        :return: Message string
-        """
-        return f"Reading: {filename}"
+    def org_not_found(self, organisation: str) -> str:
+        return f"{self.__emoji.NO_ENTRY} @{Message.colour.GREEN}{organisation}{Message.colour.WHITE} " \
+               f"(organisation) {Message.colour.RED}Not Found{Message.colour.RESET}."
 
-    @staticmethod
-    def information_not_found(param1: str, param2: str, param3: str) -> str:
-        """
-        Returns the message for information not found with the provided parameters.
+    def repo_or_user_not_found(self, repository: str, username: str) -> str:
+        return f"{self.__emoji.NO_ENTRY} {Message.colour.GREEN}{repository}{Message.colour.WHITE} -> " \
+               f"@{Message.colour.GREEN}{username}{Message.colour.WHITE} (repository/user) " \
+               f"{Message.colour.RED}Not Found{Message.colour.RESET}."
 
-        :param param1: First parameter
-        :param param2: Second parameter
-        :param param3: Third parameter
-        :return: Message string
-        """
-        return f"Information Not Found: {param1}, {param2}, {param3}"
+    def repo_does_not_have(self, title: str, repository: str, username: str) -> str:
+        return f"{self.__emoji.NO_ENTRY} {Message.colour.GREEN}{repository}{Message.colour.WHITE} -> " \
+               f"@{Message.colour.GREEN}{username} {Message.colour.WHITE}Repository does not have " \
+               f"{title}.{Message.colour.RESET}"
 
-    @staticmethod
-    def user_not_found(username: str) -> str:
-        """
-        Returns the message for user not found with the given username.
+    def prompt_log_csv(self) -> str:
+        return f"{self.__emoji.WHITE_QUESTION_MARK} " \
+               f"{Message.colour.WHITE}Would you like to log this output to a .csv file?{Message.colour.RESET}"
 
-        :param username: Username
-        :return: Message string
-        """
-        return f"User Not Found: @{username}"
+    def logged_to_csv(self, filename: str) -> str:
+        return f"{self.__emoji.GREEN_CHECK_MARK} {Message.colour.WHITE}Output logged:{Message.colour.RESET} " \
+               f"{Message.colour.GREEN}{filename}{Message.colour.RESET}"
 
-    @staticmethod
-    def org_not_found(org_name: str) -> str:
-        """
-        Returns the message for organisation not found with the given organisation name.
-
-        :param org_name: Organisation name
-        :return: Message string
-        """
-        return f"Organization Not Found: @{org_name}"
-
-    @staticmethod
-    def repo_or_user_not_found(repository: str, username: str) -> str:
-        """
-        Returns the message for repository or user not found with the provided parameters.
-
-        :param repository: Repository name
-        :param username: Username
-        :return: Message string
-        """
-        return f"Repository/User Not Found: ({repository}, @{username})"
-
-    @staticmethod
-    def repo_does_not_have_stargazers(repository: str, username: str) -> str:
-        """
-        Returns the message for 'repository does not have stargazers' with the provided parameters.
-
-        :param repository: Repository name
-        :param username: Username
-        :return: Message string
-        """
-        return f"Repository/User Not Found: ({repository}, @{username})"
-
-    @staticmethod
-    def prompt_log_csv() -> str:
-        """Returns the message for prompting whether to log output to a CSV file."""
-        return "Would you like to log this output to a .csv file?"
-
-    @staticmethod
-    def logged_to_csv(filename: str) -> str:
-        """
-        Returns the message confirming that output has been logged to a CSV file with the given filename.
-
-        :param filename: File to save the output to
-        :return: Message string
-        """
-        return f"Output logged: {filename}"
-
-    @staticmethod
-    def limit_output(title: str) -> str:
-        """
-        Returns the message for limiting output based on a user-defined value.
-
-        :param title: A title of the information that is requested to be returned
-        """
-        return f"Limit '{title}' output to how many? (1-100)"
+    def limit_output(self, title: str) -> str:
+        return f"{self.__emoji.WHITE_QUESTION_MARK} {Message.colour.WHITE}Limit{Message.colour.RESET} '{title}' " \
+               f"{Message.colour.WHITE}output to how many?{Message.colour.RESET} (1-100)"
