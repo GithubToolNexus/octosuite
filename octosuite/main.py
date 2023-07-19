0787c4c0c244d2ddde6ad8e3c82044a2ef876a9a
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2023-07-19 02:34:22 +0200

    Update main.py

--- octosuite/main.py
+++ octosuite/main.py
@@ -1,8 +1,8 @@
-import platform
+import time
 from octosuite.octosuite import xprint
 from octosuite.commands import Command
 from octosuite.messages import Message
-from octosuite.miscellaneous import path_finder, check_updates
+from octosuite.miscellaneous import clear_screen, path_finder, check_updates, systeminfo
 from octosuite.config import setup_cli_completion, setup_activity_logging, create_parser, \
     LOGS_DIRECTORY, OUTPUT_DIRECTORY, CURRENT_USERNAME
 
@@ -10,7 +10,7 @@ from octosuite.config import setup_cli_completion, setup_activity_logging, creat
 class Run:
     def __init__(self, cli_mode: bool):
         """
-        Run class constructor. Initializes parser, command, sets up directories and checks for updates.
+        Run class constructor. Initializes parser, command and sets up directories.
         
         :param cli_mode: Boolean flag to indicate whether the program is running in CLI mode.
         """
@@ -21,26 +21,53 @@ class Run:
         
         # Set up the logs and output directories.
         path_finder(directories=[LOGS_DIRECTORY, OUTPUT_DIRECTORY])
-        
-        # Check for latest program updates.
-        check_updates()
+
+        # Clear screen
+        clear_screen()
         
     def __call__(self):
         """
-        Makes the Run instance callable. Sets up command completion and handles
+        Makes the Run instance callable. Sets up command completion, initialises system information and handles
         session logging and command execution based on the mode.
         """
         # Initialise the Message instance
         message = Message()
+        system_info = systeminfo()
 
         # Set-up command completion
         setup_cli_completion()
 
+        # Set-up logging
         log = setup_activity_logging()
+
         # Log the start of a new session.
-        log.info(message.session_opened(platform.node(), CURRENT_USERNAME))
+        log.info(message.session_opened(host=system_info['node'], username=CURRENT_USERNAME, timestamp=time.asctime()))
+
+        # Log system information.
+        log.info(message.system_os(os=system_info['os']))
+        log.info(message.system_ram(ram=system_info['ram']))
+        log.info(message.system_node(node=system_info['node']))
+        log.info(message.system_release(release=system_info['release']))
+        log.info(message.system_processor(processor=system_info['processor']))
+        log.info(message.system_architecture(architecture=system_info['architecture']))
+
+        # Print the start of a session.
+        xprint(message.session_opened(host=system_info['node'], username=CURRENT_USERNAME, timestamp=time.asctime()))
+
+        # Print system information
+        xprint(message.system_os(os=system_info['os']))
+        xprint(message.system_ram(ram=system_info['ram']))
+        xprint(message.system_node(node=system_info['node']))
+        xprint(message.system_release(release=system_info['release']))
+        xprint(message.system_processor(processor=system_info['processor']))
+        xprint(message.system_architecture(architecture=system_info['architecture']))
+
         try:
             if self.cli_mode:
+
+                # Check for updates
+                check_updates()
+
                 # If in CLI mode, execute command line args
                 self.command.execute_command_line_args()
             else:
